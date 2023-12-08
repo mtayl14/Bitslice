13dfc2c29a537ab69e7c11e572a638dbf61b4d8e
Matthew Taylor <mtayl14@okstate.edu>
2023-12-08 12:43:33 -0600

    Script fixes

--- irsim/run_irsim.sh
+++ irsim/run_irsim.sh
@@ -1,37 +1,48 @@
 #!/bin/bash
 
+# Convience functions
+info() {
+	echo -e "INFO:\t$*"
+}
+
+error() {
+	echo -e "\033[31mERROR:\033[0m\t$*"
+}
+
 file_name=$1
 param_file="$PDK_ROOT/sky130A/libs.tech/irsim/sky130A_tt_nom_27.prm"
 
 # Check if irsim available
 if ! command -v irsim &> /dev/null; then
-	echo "irsim not found. Did you run eda-tools?"
+	error "'irsim' not found. Did you run eda-tools?"
 	exit 1
 fi
 
 # Check for file name
 if [ -z "$file_name" ]; then
-	echo "Please provide a file name (ex: addf)"
+	error "Please provide a file name (ex: addf)"
 	exit 1
 fi
 
 # Check for parameter file
 if [ ! -f "./sky130A_tt_nom_27.prm" ]; then
-	echo "Couldn't find SIM netlist '$file_name.sim' in this directory!"
+	error "Couldn't find SIM netlist '$file_name.sim' in this directory!"
+	info "Did you remember to generate a netlist?"
 	exit 1
 fi
 
 # Check for sim file
 if [ ! -f "$param_file" ]; then
-	echo "Couldn't find parameter file '$param_file' in this directory!"
+	error "Couldn't find parameter file '$param_file' in this directory!"
 	exit 1
 fi
 
 # Check for cmd file
 if [ ! -f "./$file_name.cmd" ]; then
-	echo "Couldn't find IRSIM command file '$file_name.cmd' in this directory!"
+	error "Couldn't find IRSIM command file '$file_name.cmd' in this directory!"
 	exit 1
 fi
 
 # Run IRSIM
+info "Simulating circuit '$file_name' with 'irsim'..."
 irsim "$param_file" "$file_name.sim" "-$file_name.cmd"
--- netgen/do_lvs.sh
+++ netgen/do_lvs.sh
@@ -1,39 +1,79 @@
 #!/bin/bash
 
+# Convience functions
+info() {
+	echo -e "INFO:\t$*"
+}
+
+error() {
+	echo -e "\033[31mERROR:\033[0m\t$*"
+}
+
 file_name=$1
 circuit_name=$2
 
 # Check if netgen available
 if ! command -v netgen &> /dev/null; then
-	echo "netgen not found. Did you run eda-tools?"
+	error "netgen not found. Did you run eda-tools?"
 	exit 1
 fi
 
 # Check for file name
 if [ -z "$file_name" ]; then
-	echo "Please provide a file name (ex: addf)"
+	error "Please provide a file name (ex: addf)"
 	exit 1
 fi
 
 # If a specific circuit is not specified, use the file name
 if [ -z "$circuit_name" ]; then
+	info "Using default circuit name '$file_name'"
 	circuit_name=$file_name
 fi
 
 # Check for spice file from xschem
 if [ ! -f "../xschem/$file_name.spice" ]; then
-	echo "Couldn't find '$file_name.spice' in xschem directory!"
-	echo "Did you remember to click the netlist button in xschem?"
+	error "Couldn't find '$file_name.spice' in xschem directory!"
+	info "Did you remember to click the netlist button in xschem?"
 	exit 1
 fi
 
 # Check for spice file from magic
 if [ ! -f "../magic/$file_name.spice" ]; then
-	echo "Couldn't find '$file_name.spice' in the magic directory!"
-	echo "Did you remember to run `ext4mag`?"
-	exit 1
+	error "Couldn't find '$file_name.spice' in the magic directory!"
+	
+	# Does the cell even exist?
+	if [ ! -f "../magic/$file_name.mag" ]; then
+		error "Couldn't find '$file_name.mag' in the magic directory!"
+		error "Can't find cell for LVS."
+		exit 1
+	fi
+
+	info "Found '$file_name.mag' in the magic directory!"
+	info "Attempting 'ext4mag' for '$file_name.mag'..."
+
+	# Check if ext4mag available
+	if ! command -v ext4mag &> /dev/null; then
+		error "Error: 'ext4mag' not found. Did you run eda-tools?"
+		exit 1
+	fi
+	
+	# Save the current directory
+	old_pwd=$PWD
+
+	# Go to magic directory
+	cd "$PWD/../magic/"
+
+	# Run ext4mag
+	ext4mag "$file_name"
+
+	# ext4mag returns before completion, so sleep to let it finish
+	sleep 3
+
+	# Return to this directory
+	cd "$old_pwd"
+	info "Done with 'ext4mag'."
 fi
 
 # Run LVS and place output in current directory
-echo "Running LVS for circuit '$circuit_name' from file '$file_name'…"
+info "Running LVS for circuit '$circuit_name' from file '$file_name'…"
 netgen -batch lvs "../magic/$file_name.spice $circuit_name" "../xschem/$file_name.spice $circuit_name" $PDK_ROOT/sky130A/libs.tech/netgen/sky130A_setup.tcl $1.out
